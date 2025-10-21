b9357bbcbc0199d4f7baa3188023897550a1af65
Himanshu Makwana <hm896292@gmail.com>
2025-10-21 23:55:31 +0530

    gh pages change

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+    base: '/emma-liu-creations/', // IMPORTANT: Add this
   server: {
     host: "::",
     port: 8080,
