4bfef5f5d96bd6a27111c9cfe2a3d57f751199d8
Himanshu Makwana <hm896292@gmail.com>
2025-10-22 00:17:29 +0530

    small change

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: '/portfolio/', // IMPORTANT: Add this
+    // base: '/portfolio/', // IMPORTANT: Add this
   server: {
     host: "::",
     port: 8080,
