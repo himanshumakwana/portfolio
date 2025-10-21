2018f554767062566e0c11ec89f84249f74d5ada
Himanshu Makwana <hm896292@gmail.com>
2025-10-22 00:13:31 +0530

    repo name change

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: '/emma-liu-creations/', // IMPORTANT: Add this
+    base: '/portfolio/', // IMPORTANT: Add this
   server: {
     host: "::",
     port: 8080,
